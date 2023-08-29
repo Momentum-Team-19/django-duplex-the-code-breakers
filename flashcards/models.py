737be9155f92de9088675a4cec23ecc2cfa67e1a
Chris Riddle <stopimmediatley@gmail.com>
2023-08-29 10:23:39 -0400

    fixing pip

--- flashcards/models.py
+++ flashcards/models.py
@@ -8,16 +8,24 @@ from django.contib.auth.models import AbstractUser
 class Deck(models.Model):
     title = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
-    pass
+
+    def archive(self):
+        self.created_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
 
 
 class Card(models.Model):
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE, related_name="decks")
     prompt = models.CharField(max_length=200)
     answer = models.CharField(max_length=200)
-    pass
+    
+    def __str__(self):
+        return self.prompt
+        
 
 
 class User(AbstractUser):
     pass
-
