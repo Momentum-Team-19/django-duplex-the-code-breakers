5aedf387327708f762c0756760070ca00e608e18
davis <davisfpatterson@gmail.com>
2023-09-01 00:36:28 -0400

    fixed styling, the 'yes' button in the 'study_again' page is overlapping with the text above it still

--- config/urls.py
+++ config/urls.py
@@ -36,5 +36,5 @@ urlpatterns = [
     path('card/edit/<int:pk>', views.edit_card, name='edit_card'),
     path('deck/study/<int:pk>', views.study, name='study'),
     path('correct/<int:pk>/', views.toggle_correct, name='toggle_correct'),
-    path('deck/studyagain/', views.study, name='study_again'),
+    path('deck/study/again/', views.refresh_deck, name='study_again'),
 ]
